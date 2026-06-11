101aaa1699ad62e259523386869a1b2d4d44276d
tiktokjeton <tiktokjeton5k@gmail.com>
2026-06-11 04:05:11 +0400

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ mongoose.connect(dbURI)
   .then(() => console.log('Baza aktivdir!'))
   .catch(err => console.log('Xəta:', err));
 
-// İstifadəçi modeli
+// İstifadəçi modeli (məlumatları bazada saxlayır)
 const UserSchema = new mongoose.Schema({
     username: String,
     password: String,
@@ -31,13 +31,15 @@ const activeSockets = {};
 io.on('connection', (socket) => {
     socket.on('auth', async (data) => {
         const { type, username, password } = data;
+        
         if (type === 'register') {
             const existing = await User.findOne({ username });
             if (existing) return socket.emit('auth_response', { success: false, message: 'İstifadəçi var!' });
             await User.create({ username, password, avatar: '', following: [], followers: [] });
             activeSockets[socket.id] = username;
             socket.emit('auth_response', { success: true, username });
-        } else if (type === 'login') {
+        } 
+        else if (type === 'login') {
             const user = await User.findOne({ username, password });
             if (!user) return socket.emit('auth_response', { success: false, message: 'Səhv məlumat!' });
             activeSockets[socket.id] = username;
